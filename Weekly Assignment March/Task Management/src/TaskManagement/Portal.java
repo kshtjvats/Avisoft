bfaa8f457d66c053392b8a492e573b9e0964e6c6
kshtjvats <Kshitij.vats@avisoft.io>
2024-03-18 11:28:12 +0530

    Task Management

--- Weekly Assignment March/Task Management/src/TaskManagement/Portal.java
+++ Weekly Assignment March/Task Management/src/TaskManagement/Portal.java
@@ -1,24 +1,48 @@
 package TaskManagement;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Portal {
 static Scanner scanner=new Scanner(System.in);
-public static void main(String[] args) {
+public static void main(String[] args) throws IOException {
     List<Student>listOfStudents=new ArrayList<Student>();
+    int count=0;
+    Set<String>emails=new HashSet<String>();
     int choiceForSwitch=0;
-    while(choiceForSwitch!=3)
+    while(choiceForSwitch!=4)
     {
-        System.out.println("Enter a choice\n1 : Admin Login\n2 : Student Login\n3 : Quit");
+        System.out.println("Enter a choice\n1 : Admin Login\n2 : Student Registeration\n3:  Registeration\n4 : Quit");
         choiceForSwitch=scanner.nextInt();
         switch (choiceForSwitch) {
             case 1:
                 
             break;
             case 2:
-                
+            System.out.println("Enter your Email");
+            String email=scanner.next();
+            if(!emails.contains(email))
+            {
+            emails.add(email);
+            System.out.println("Enter your name");
+            String name=scanner.next();
+            System.out.println("Enter Password");
+            String password1=scanner.next();
+            System.out.println("Enter your Batch");
+            String Batch=scanner.next();
+            Student student=new Student(name,email,password1,Batch);
+            listOfStudents.add(student);
+            AddStudentToList(count,student);
+            System.out.println("Student Successfully Registered");
+            count++;
+            }
+            else
+            System.out.println("A student with this credential exist!");
             break;
             case 3:
                 
@@ -27,5 +51,19 @@ public static void main(String[] args) {
                 break;
         }
     }
+}
+static void AddStudentToList(int count,Student student) throws IOException
+{
+FileWriter filewriter=new FileWriter("src/TaskManagement/list.csv",true);
+if(count==0)
+{
+filewriter.write("Name,Email,Batch\n"); 
+filewriter.write(student.getName()+","+student.getEmail()+","+student.getBatch());
+}
+else
+{
+filewriter.write(student.getName()+","+student.getEmail()+","+student.getBatch());
+}
+filewriter.close();
 }
 }
--- Weekly Assignment March/Task Management/src/TaskManagement/Student.java
+++ Weekly Assignment March/Task Management/src/TaskManagement/Student.java
@@ -5,9 +5,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Student {
-    Scanner scanner=new Scanner(System.in);
+Scanner scanner=new Scanner(System.in);
 private String name,email,password,batch;
 List<Task>myTasks;
+List<Student>studentList=new ArrayList<Student>();
 Student(String name,String email,String password,String batch)
 {
     this.name=name;
@@ -19,7 +20,21 @@ void assignTask(List<Task>Tasks)
 {
     this.myTasks=Tasks;
 }
+String getName()
+{
+    return name;
+}
+String getEmail()
+{
+    return email;
+}
+String getBatch()
+{
+    return batch;
+}
 void SubmitTask()
+{
+if(myTasks.size()!=0)
 {
 System.out.println("Enter the serial number corresponding to Task");
 int serialNumber=scanner.nextInt();
@@ -35,6 +50,9 @@ else
     if(command==1)
     myTasks.get(serialNumber).setDoneStatus();
 }
+}
+else
+System.out.println("No tasks to submit");
 }
 void showUpcomingWork(List<Task>myTasks)
 {
@@ -57,5 +75,17 @@ void showUpcomingWork(List<Task>myTasks)
         oneMinuteThread.start();
         }
     }
+}
+Student addStudent()
+{
+System.out.println("Enter Student email");
+String email=scanner.next();
+System.out.println("Enter Student name");
+String name=scanner.next();
+System.out.println("Enter Password");
+String password=scanner.next();
+System.out.println("Enter Batch");
+String batch=scanner.next();
+return new Student(name,email,password,batch);
 }
 }
